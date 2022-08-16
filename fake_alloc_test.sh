c091dbff70c5a5be5b6f61eff99d26b2ec3b1a36
Qidong Su <soodoshll@gmail.com>
2022-08-16 15:09:26 -0400

    update

--- fake_alloc_test.sh
+++ fake_alloc_test.sh
@@ -1,6 +1,6 @@
-for bs in $(seq 10 10 300)
+for bs in $(seq 1 1 100)
 do
-    cmd="python test_amp.py --amp 1 "$bs
+    cmd="python run_vgg.py --model vgg16 "$bs
     mem=$(eval LD_PRELOAD=./fake_libcudart.so PYTORCH_NO_CUDA_MEMORY_CACHING=1 $cmd "2>/dev/null")
     ret=$?
     mem=$(tail -n 1 <<< $mem)
@@ -11,17 +11,17 @@ do
         mem1="fail"
     fi
     
-    mem=$(eval $cmd "2>/dev/null")
-    ret=$?
-    mem=$(tail -n 1 <<< $mem)
-    if (($ret == 0))
-    then
-        mem2=$mem
-    else
-        mem2="fail"
-    fi
+    # mem=$(eval $cmd "2>/dev/null")
+    # ret=$?
+    # mem=$(tail -n 1 <<< $mem)
+    # if (($ret == 0))
+    # then
+    #     mem2=$mem
+    # else
+    #     mem2="fail"
+    # fi
 
-    echo $bs, $mem1, $mem2
+    echo $bs, $mem1
 done
 
 # for n_layers in $(seq 50 10 100)
--- run_vgg.py
+++ run_vgg.py
@@ -9,18 +9,22 @@ from apex import amp
 import torchvision
 # from functorch import make_functional
 # from functorch.compile import aot_module, min_cut_rematerialization_partition, nop, memory_efficient_fusion
+import faulthandler
+faulthandler.enable()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('batch_size', type=int)
 parser.add_argument('--amp', type=int, default=0)
+parser.add_argument('--model', type=str, default="vgg16")
 args = parser.parse_args()
 
 use_fake_alloc = os.environ.get("LD_PRELOAD", None) == "./fake_libcudart.so"
 print("Using fake allocator:", use_fake_alloc)
 if use_fake_alloc:
     import fake_alloc
+    fake_alloc.set_target_mem_limit(16_000_000_000)
 # 
-    torch.backends.cudnn.benchmark = False
+# torch.backends.cudnn.benchmark = False
 # torch.backends.cudnn.deterministic = True
 
 # print(torch.backends.cudnn.version())
@@ -29,10 +33,40 @@ if use_fake_alloc:
 # torch.backends.cudnn.enabled = False
 
 # model = build_vgg_model()
-model = torchvision.models.resnet50()
+# class SavedTensorRecord(object):
+#     saved_tensor_size = 0
+
+#     @property
+#     def pack_hook(self):
+#         def foo(x):
+#             self.saved_tensor_size += x.element_size() * x.nelement()
+#             return x
+#         return foo
+
+#     @property
+#     def unpack_hook(self):
+#         def foo(x):
+#             return x
+#         return foo
+    
+#     def reset(self):
+#         self.saved_tensor_size = 0
+
+# saved_tensor_record = SavedTensorRecord()
+
+if args.model == 'vgg':
+    model = build_vgg_model(False)
+else:
+    model = getattr(torchvision.models, args.model)()
 device = torch.device('cuda')
 model.to(device)
 print("model created")
+
+parameter_size = 0
+for p in model.parameters():
+    parameter_size += p.data.element_size() * p.data.nelement()
+
+print("parameters: ", parameter_size)
 
 # while True:
 #     pass
@@ -46,28 +80,47 @@ bs = args.batch_size
 image_size = 224
 x = torch.rand((bs, 3, image_size, image_size), device=device)
 model(x)
+
 if use_fake_alloc:
     fake_alloc.init_max_mem()
 else:
     torch.cuda.reset_max_memory_allocated()
     
 t = torch.randint(1000, (bs, ), device=device)
-# loss_fn = who_cares_loss
 loss_fn = torch.nn.CrossEntropyLoss()
 optim = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 # model, optim = amp.initialize(model, optim, opt_level="O" + str(args.amp))
 
-optim.zero_grad()
-print("run forward")
-out = model(x)
-# out = compiled_model(x)
-loss = loss_fn(out, t)
-# loss = torch.sum(out)
-# with amp.scale_loss(loss, optim, delay_overflow_check=True) as scaled_loss:
-loss.backward()
-optim.step()
 
+for i in range(5):
+    optim.zero_grad(set_to_none=True)
+    # saved_tensor_record.reset()
+    if i == 1:
+        torch.cuda.synchronize()
+        print("No benchmark anymore!")
+    if i == 4:
+        torch.cuda.synchronize()
+        if use_fake_alloc:
+            fake_alloc.init_max_mem()
+        else:
+            torch.cuda.reset_peak_memory_stats()
+    # print("run forward")
+    # with torch.no_grad():
+    # with torch.autograd.graph.saved_tensors_hooks(saved_tensor_record.pack_hook, saved_tensor_record.unpack_hook):
+    out = model(x)
+    # loss = loss_fn(out, t)
+    loss = out.sum()
+    # with amp.scale_loss(loss, optim, delay_overflow_check=True) as scaled_loss:
+    # print("run backward")
+    loss.backward()
+    optim.step()
+        # del out,loss
+    # torch.cuda.empty_cache()
+
+del x, t, model
+torch.cuda.synchronize()
+# print(saved_tensor_record.saved_tensor_size)
 if use_fake_alloc:
     print(fake_alloc.max_mem_allocated())
 else:
