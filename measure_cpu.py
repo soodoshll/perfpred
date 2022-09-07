73bdb04035c454dda1f6069fd2d8a338547304d2
Qidong Su <soodoshll@gmail.com>
2022-09-07 00:38:50 -0400

    update

--- measure_cpu.py
+++ measure_cpu.py
@@ -4,6 +4,13 @@ import numpy as np
 
 from measure import measure_op, ConvMeasure, MatMulMeasure, BatchNormMeasure
 from torch.autograd import DeviceType
+from matplotlib import pyplot as plt
+import argparse, pickle
+
+parser = argparse.ArgumentParser()
+parser.add_argument("op", choices=["conv2d", "mm", "batchnorm", "all"])
+parser.add_argument("command", choices=["measure", "plot", "measure_time"])
+args = parser.parse_args()
 
 def _find_next(events, last):
     for evt in events:
@@ -17,8 +24,6 @@ class ConvMeasureCPU(ConvMeasure):
             layer = data[1]
             layer.train()
             out = layer(A)
-            # out = out.sum()
-            # out.backward()
             torch.cuda.synchronize(self.device)
         return func
 
@@ -34,9 +39,8 @@ class ConvMeasureCPU(ConvMeasure):
                 if evt.name == 'aten::conv2d':
                     dur = evt.cpu_time_total
                     real_dur = _find_next(events, evt).time_range.start - evt.time_range.start
-                    print(self.params, "#", real_dur)
                     real_durs.append(real_dur)
-            # info['data'].append()
+                    info['data'].append(real_dur)
         return func
 
 class MatMulMeasureCPU(MatMulMeasure):
@@ -45,9 +49,6 @@ class MatMulMeasureCPU(MatMulMeasure):
             A = data[0]
             B = data[1]
             C = A.matmul(B)
-            # out = out.sum()
-            # out.backward()
-            # torch.cuda.synchronize(self.device)
         return func
 
     def get_analyze_func(self):
@@ -62,8 +63,9 @@ class MatMulMeasureCPU(MatMulMeasure):
                 if evt.name == 'aten::mm':
                     dur = evt.cpu_time_total
                     real_dur = _find_next(events, evt).time_range.start - evt.time_range.start
-                    print(self.params, "#", real_dur)
+                    # print(self.params, "#", real_dur)
                     real_durs.append(real_dur)
+                    info['data'].append(real_dur)
         return func    
 
 class BatchNormMeasureCPU(BatchNormMeasure):
@@ -90,41 +92,79 @@ class BatchNormMeasureCPU(BatchNormMeasure):
                 if evt.name == 'aten::batch_norm':
                     dur = evt.cpu_time_total
                     real_dur = _find_next(events, evt).time_range.start - evt.time_range.start
-                    print(self.params, "#", real_dur)
+                    # print(self.params, "#", real_dur)
                     real_durs.append(real_dur)
+                    info['data'].append(real_dur)
         return func 
 
-# batchnorm_measure_cpu = BatchNormMeasureCPU(
-#     batch_size_range=(1, 64),
-#     image_size_range=(2, 224),
-#     channels_range=(1, 1024),
-#     device=torch.device(f'cuda:0')
-# )
+def read_data(filename):
+    durs = []
+    with open(filename, "rb") as f:
+        while 1:
+            try:
+                objs = pickle.load(f)
+                for obj in objs:
+                    durs.append(obj / 1000)
+            except (EOFError):
+                break
+    durs = np.array(durs)
+    return durs
 
-# batchnorm_measure_cpu.run(1000, filename="nomatter")
+CONV_FILENAME= "data/conv_data_cpu.data"
+MM_FILENAME = "data/mm_data_cpu.data"
+BATCHNORM_FILENAME="data/batchnorm_cpu.data"
 
-# exit(0)
+fn_dict = {
+    "conv2d": CONV_FILENAME,
+    "mm": MM_FILENAME,
+    "batchnorm": BATCHNORM_FILENAME
+}
 
-# matmul_measure_cpu = MatMulMeasureCPU(
-#     n_range=(1, 1024),
-#     m_range=(1, 16384),
-#     k_range=(1, 16384),
-#     device=torch.device('cuda:0')
-# )
+if args.command == "measure":
+    if args.op == "conv2d":
+        conv_measure_cpu = ConvMeasureCPU(
+            batch_size_range=(1, 48),
+            image_size_range=(2, 224),
+            in_channels_range=(3, 1024),
+            out_channels_range=(16, 1024),
+            kernel_size_range=(1, 7),
+            stride_range=(1, 7),
+            padding_range=(1, 3),
+            device=torch.device('cuda:0')
+        )
+        conv_measure_cpu.run(1000, dx=True, filename=CONV_FILENAME)
+    elif args.op == "mm":
+        matmul_measure_cpu = MatMulMeasureCPU(
+            n_range=(1, 1024),
+            m_range=(1, 16384),
+            k_range=(1, 16384),
+            device=torch.device('cuda:0')
+        )
+        matmul_measure_cpu.run(1000, filename=MM_FILENAME)
+    elif args.op == "batchnorm":
+        batchnorm_measure_cpu = BatchNormMeasureCPU(
+            batch_size_range=(1, 64),
+            image_size_range=(2, 224),
+            channels_range=(1, 1024),
+            device=torch.device(f'cuda:0')
+        )
+        batchnorm_measure_cpu.run(1000, filename=BATCHNORM_FILENAME)
+    else:
+        raise RuntimeError("Not Supported")
+elif args.command == "plot":
+    if args.op == "all":
+        conv_data = read_data(CONV_FILENAME)
+        mm_data = read_data(MM_FILENAME)
+        bn_data = read_data(BATCHNORM_FILENAME)
+        labels = ["conv2d", "matmul", "batch_norm"]
 
-# matmul_measure_cpu.run(1000, filename='data/matmul_data_cpu.data')
-
-# exit(0)
-
-conv_measure_cpu = ConvMeasureCPU(
-    batch_size_range=(1, 64),
-    image_size_range=(2, 224),
-    in_channels_range=(3, 1024),
-    out_channels_range=(16, 1024),
-    kernel_size_range=(1, 7),
-    stride_range=(1, 7),
-    padding_range=(1, 3),
-    device=torch.device('cuda:0')
-)
-
-conv_measure_cpu.run(1000, dx=True, filename='data/conv_data_cpu.data')
+        plt.violinplot([conv_data, mm_data, bn_data], showmeans=True, showextrema=False, showmedians=True)
+        plt.xticks(np.arange(1, len(labels) + 1), labels=labels)
+        plt.xlim(0.25, len(labels) + 0.75)
+        plt.ylim(0, 2)
+        plt.ylabel('time (ns)')
+        plt.savefig("figure/cpu_overhead.png")
+    else:
+        data = read_data(fn_dict[args.op])
+        plt.violinplot(data, showextrema=False, showmeans=True)
+        plt.savefig("figure/cpu_overhead_e2e.png")
--- nvfuser_vision.py
+++ nvfuser_vision.py
@@ -3,6 +3,7 @@ import torchvision
 import torch.nn.functional as F
 import time
 import numpy as np
+from utils import timing
 device = 'cuda'
 
 
@@ -24,59 +25,55 @@ class ResBlock(torch.nn.Module):
         return o
 
 # model = ResBlock(64, 64)
-x = torch.rand(32, 3, 224, 224, device=device)
-model = torchvision.models.resnet50()
-model.to(device)
-model.eval()
+nitr = 20
 
-for _ in range(3):
-    model(x)
-torch.cuda.synchronize()
-t0 = time.time()
-for _ in range(20):
-    model(x)
-torch.cuda.synchronize()
-dur = time.time() - t0
-print("before compiled:", dur/20)
+for model_name in ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152"]:
+    bs = 32
+    x = torch.rand(bs, 3, 224, 224, device=device)
+    model = getattr(torchvision.models, model_name)()
+    model.to(device)
+    model.eval()
 
-traced_model = torch.jit.trace(model, (x, ))
-print("tracing completes")
+    for _ in range(3):
+        model(x)
+    dur = timing(lambda : model(x), nitr)
+    # print("before compiled:", dur/50)
 
-for _ in range(3):
-    traced_model(x)
-print("start profiling compiled version")
-torch.cuda.synchronize()
-t0 = time.time()
-for _ in range(20):
-    traced_model(x)
-torch.cuda.synchronize()
-dur = time.time() - t0
+    traced_model = torch.jit.trace(model, (x, ))
+    # print("tracing completes")
 
-print("after compiled:", dur/20)
+    for _ in range(3):
+        traced_model(x)
+    # print("start profiling compiled version")
+    dur1 = timing(lambda : traced_model(x), nitr)
+    # print("after compiled:", dur1/50)
 
-graph = traced_model.graph_for(x)
-group_nodes = graph.findAllNodes("prim::CudaFusionGroup")
-groups = []
+    graph = traced_model.graph_for(x)
+    group_nodes = graph.findAllNodes("prim::CudaFusionGroup")
+    groups = []
 
-kind_counted = [
-    "aten::batch_norm",
-    "aten::relu"
-    "aten::add"
-]
+    kind_counted = [
+        "aten::batch_norm",
+        "aten::relu"
+        "aten::add"
+    ]
 
-io_size = 0
+    io_size = 0
 
-for node in group_nodes:
-    groups.append(node.g("Subgraph"))
-for group in groups:
-    g_list = list(group.nodes())
-    for i, node in enumerate(g_list):
-        if node.kind() in kind_counted:
-            io_amount = np.prod(node.output().type().sizes())
-            if i == 0 or i == len(g_list) - 1:
-                io_size += io_amount
-            else:
-                io_size += 2 * io_amount
+    for node in group_nodes:
+        groups.append(node.g("Subgraph"))
+    for group in groups:
+        g_list = list(group.nodes())
+        for i, node in enumerate(g_list):
+            # print(node.kind())
+            # if node.kind() != "prim::Constant":
+                # print(node)
+            if node.kind() in kind_counted:
+                io_amount = np.prod(node.output().type().sizes())
+                if i == 0 or i == len(g_list) - 1:
+                    io_size += io_amount
+                else:
+                    io_size += 2 * io_amount
 
-print(io_size)
-print(io_size * 4 / 1e9 / 448)
+    # print(io_size)
+    print(f"{model_name}, {dur}, {dur1}, {io_size * 4 / 1e9 / 448 * 1000}")
