c53c98825ab932e2e628d7ec9794ec43efdfdfb8
Qidong Su <soodoshll@gmail.com>
2022-12-12 03:02:24 -0500

    update

--- scripts/mem/mlp.sh
+++ scripts/mem/mlp.sh
@@ -2,13 +2,14 @@
 cmd="python scripts/mem/mlp.py --nlayer 200 --ndim 2048"
 if [ "$FAKE_ALLOC" == 1 ]
 then
+    echo "Using fake allocator"
     source ./faketorch/bin/activate
-    preload_path=./fake_libcudart.so
+    cmd="env LD_PRELOAD=./fake_libcudart.so "$cmd
 fi
 
 for bs in {1024..8192..512}
 do
-    ret=$(env LD_PRELOAD=$preload_path $cmd --batch_size $bs 2>/dev/null)
+    ret=$($cmd --batch_size $bs 2>/dev/null)
     echo $bs, $ret
     sleep 1
 done
