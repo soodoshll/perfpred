0018058283629e4c0a3a0750accd20cf41a9641c
anatanopapa <bugaosuni@bugaosuni>
2022-12-12 00:58:16 -0500

    update

--- scripts/mem/mlp.py
+++ scripts/mem/mlp.py
@@ -6,6 +6,7 @@ from perfpred.utils import remove_initialization, measure_gpu_mem
 import os
 import subprocess
 from subprocess import PIPE, STDOUT
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ndim', type=int, default=1024)
@@ -48,10 +49,6 @@ def train(nitr=2):
     
     torch.cuda.synchronize()
 
-if os.path.isfile("log_mem.tmp"):
-    os.remove("log_mem.tmp")
-
-pid = os.getpid()
 
 if use_fake_alloc:
     fake_alloc.set_target_mem_limit(24 * 1024 * 1024 * 1024)
@@ -59,14 +56,20 @@ if use_fake_alloc:
     train(args.nitr)
     print(fake_alloc.max_mem_allocated() / (1024)**2)
 else:
+    if os.path.isfile("log_mem.tmp"):
+        os.remove("log_mem.tmp")
+    pid = os.getpid()
     monitor_proc = subprocess.Popen(['nvidia-smi', '-lms', '10', '--query-compute-apps=pid,used_gpu_memory', '--format=csv', '-f', 'log_mem.tmp'])
+    # monitor_proc = subprocess.Popen(['nvidia-smi', '-lms', '10', '--query-compute-apps=pid,used_gpu_memory', '--format=csv,noheader',], stdout=PIPE)
     train(args.nitr)
     monitor_proc.terminate()
+    time.sleep(1)
     max_mem = 0
     with open("log_mem.tmp", "r") as f:
+        # for line in monitor_proc.communicate()[0].splitlines():
         for line in f:
             tokens = line.split(',')
-            if int(tokens[0]) == pid:
+            if tokens[0] == str(pid):
                 mem = int(tokens[1].split()[0])
                 max_mem = max(max_mem, mem)
     os.remove("log_mem.tmp")
