a749e410a77757fe6399cb1add187a04db47eef5
Qidong <soodoshll@gmail.com>
2022-12-18 01:44:43 -0500

    update

---
+++ scripts/time/batchsize_cpu_gpu.py
@@ -0,0 +1,86 @@
+import torch
+from torch import nn
+import os
+import argparse
+import time
+import torchvision
+from torch.autograd import DeviceType
+from perfpred.measure import get_children_kernel_time2
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('batch_size', type=int)
+parser.add_argument('--model', type=str, default="vgg13")
+args = parser.parse_args()
+
+model = getattr(torchvision.models, args.model)()
+device = torch.device('cuda')
+model.to(device)
+
+def _get_all_children(events, root):
+    ret = []
+    for evt in events:
+        if evt.time_range.start >= root.time_range.start and evt.time_range.end <= root.time_range.end:
+            ret.append(evt)
+    return ret
+
+def _get_children_kernel_time(event, marked_kernel=None):
+    kernel_time = sum([kernel.duration for kernel in event.kernels])
+    if marked_kernel is not None:
+        marked_kernel.update(event.kernels)
+    for child in event.cpu_children:
+        kernel_time += _get_children_kernel_time(child, marked_kernel)
+    return kernel_time
+
+def analyze_profile(prof):
+    events = prof.profiler.function_events
+    cnt = 0
+    for evt in events:
+        if evt.name == 'ProfilerStep*':
+            cnt += 1
+            if cnt == 0:
+                continue
+            children = _get_all_children(events, evt)
+            gpu_time = 0
+            cpu_time = total_time = evt.cpu_time_total
+            for child in children:
+                if child.name.startswith("cudaDeviceSynchronize"):
+                    cpu_time -= child.cpu_time_total
+                kernel_time = sum([kernel.duration for kernel in child.kernels])
+                gpu_time += kernel_time
+            total_times.append(total_time)
+            gpu_times.append(gpu_time)
+            cpu_times.append(cpu_time)
+
+bs = args.batch_size
+image_size = 224
+x = torch.rand((bs, 3, image_size, image_size), device=device)
+def train_loop():
+    optim.zero_grad(set_to_none=False)
+    out = model(x)
+    loss = loss_fn(out, t)
+    loss.backward()
+    optim.step()
+    torch.cuda.synchronize()
+
+cpu_times = []
+gpu_times = []
+total_times = []
+
+t = torch.randint(1000, (bs, ), device=device)
+loss_fn = torch.nn.CrossEntropyLoss()
+optim = torch.optim.SGD(model.parameters(), lr=1e-3)
+with torch.profiler.profile(
+    schedule=torch.profiler.schedule(
+        wait=2,
+        warmup=2,
+        active=6,
+        repeat=1),
+    on_trace_ready=analyze_profile,
+    with_stack=True
+) as profiler:
+    for i in range(15):
+        train_loop()
+        profiler.step()
+
+print(f"{np.mean(cpu_times)/1e3 : .2f}, {np.mean(gpu_times)/1e3 : .2f}, {np.mean(total_times)/1e3 :.2f}")
