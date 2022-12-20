a42fb774e03fb8d5bf5a08845bb4bf623cdc7a55
Qidong <soodoshll@gmail.com>
2022-12-19 20:08:23 -0500

    update

--- scripts/mem/transformer.sh
+++ scripts/mem/transformer.sh
@@ -8,26 +8,33 @@ then
     cmd="env LD_PRELOAD=./fake_libcudart.so "$cmd
 fi
 
-for bs in 2 4 8
-do
-    model=bert-large-uncased
+for bs in {1..32..1}
+    model=bert-base-uncased
     ret=$($cmd --model $model --seq_len 512 --batch_size $bs 2>/dev/null)
     echo $model, $bs, $ret
     sleep 1
 done
 
-for bs in 2 4 6
-do
-    model=gpt2-medium
-    ret=$($cmd --model $model --seq_len 512 --batch_size $bs 2>/dev/null)
-    echo $model, $bs, $ret
-    sleep 1
-done
+# for bs in 2 4 8
+# do
+#     model=bert-large-uncased
+#     ret=$($cmd --model $model --seq_len 512 --batch_size $bs 2>/dev/null)
+#     echo $model, $bs, $ret
+#     sleep 1
+# done
 
-for bs in 4 8 12
-do
-    model=albert-large-v2
-    ret=$($cmd --model $model --seq_len 512 --batch_size $bs 2>/dev/null)
-    echo $model, $bs, $ret
-    sleep 1
-done
+# for bs in 2 4 6
+# do
+#     model=gpt2-medium
+#     ret=$($cmd --model $model --seq_len 512 --batch_size $bs 2>/dev/null)
+#     echo $model, $bs, $ret
+#     sleep 1
+# done
+
+# for bs in 4 8 12
+# do
+#     model=albert-large-v2
+#     ret=$($cmd --model $model --seq_len 512 --batch_size $bs 2>/dev/null)
+#     echo $model, $bs, $ret
+#     sleep 1
+# done
