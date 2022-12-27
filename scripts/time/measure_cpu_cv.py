78330e7d6bac8daac69b92f302ce760ae88ab3a4
Qidong <soodoshll@gmail.com>
2022-12-26 23:54:24 -0500

    update

--- scripts/time/measure_cpu_cv.py
+++ scripts/time/measure_cpu_cv.py
@@ -11,8 +11,8 @@ device = 'cuda'
 model = torchvision.models.resnet18()
 model.to(device)
 
-inputs = torch.empty((16, 3, 224, 224), device=device)
-labels = torch.zeros((16, ), dtype=torch.int64, device=device)
+inputs = torch.empty((1, 3, 224, 224), device=device)
+labels = torch.zeros((1, ), dtype=torch.int64, device=device)
 
 optim = torch.optim.SGD(model.parameters(), lr=1e-3)
 loss_fn = torch.nn.CrossEntropyLoss()
@@ -27,15 +27,44 @@ def train_loop():
 events = profile_model(train_loop)
 evt_time_dict = {}
 
+start_time = []
+end_time = []
+
+def _add_event(evt):
+    start_time.append(evt.time_range.start)
+    end_time.append(evt.time_range.end)
+    if evt.name in evt_time_dict:
+        evt_time_dict[evt.name].append(evt.cpu_time_total)
+    else:
+        evt_time_dict[evt.name] = []
+
+tot_time = []
 
 for evt in events:
     if evt.name.startswith("ProfilerStep*"):
         first_level_ops = _get_first_level_ops(events, evt)
+        tot_time.append(evt.cpu_time_total)
         for op in first_level_ops:
-            if op.name in evt_time_dict:
-                evt_time_dict[op.name].append(op.cpu_time_total)
+            if op == evt:
+                continue
+            if op.name.startswith('Optimizer.step'):
+                for c in op.cpu_children:
+                    _add_event(c)
+            elif op.name.startswith('Optimizer.zero_grad'):
+                for c in op.cpu_children:
+                    _add_event(c)
+                    # print(c.name)
             else:
-                evt_time_dict[op.name] = []
+                _add_event(op)
+
+tot_gap = 0            
+for i in range(len(start_time) - 1):
+    gap = start_time[i + 1] - end_time[i]
+    tot_gap += gap
+
 
 for op, times in evt_time_dict.items():
    print(op, np.mean(times), np.std(times)) 
+
+print("GAP:", tot_gap/1e3)
+print("TOT:", np.mean(tot_time)/1e3)
